432740657b3fd5574d37cba9385c69bd7c9f3a92
Kirillkp59 <67429794+Kirillkp59@users.noreply.github.com>
2020-09-09 16:56:24 +0300

    Update main.cpp

--- Code Generator/main.cpp
+++ Code Generator/main.cpp
@@ -1,11 +1,38 @@
-#include "iostream"
-#include <windows.h>
-#include "fstream"
-#include "conio"
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <fstream>
 
-using std::cout
+using namespace std;
 
-int main(){
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-  return 0;
+	system("color 0a");
+
+	string line;
+	ifstream in("code.txt");
+	if (in.is_open()) {
+		while (getline(in, line)) {
+			for (int i = 0; i < line.size(); i++) {
+				_getch();
+				Sleep(5);
+				cout << line[i];
+			}
+			cout << endl;
+		}
+	}
+	else {
+		cout << "File not found (code.txt)!" << endl;
+		cout << "Creating a new file (code.txt)!" << endl;
+		ofstream newfile ("code.txt");
+			newfile << "Put the code here" << endl;
+		newfile.close();
+	}
+	Sleep(5000);
 }
+
